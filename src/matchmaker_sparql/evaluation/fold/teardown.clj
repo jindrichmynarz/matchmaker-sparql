b1abddc9159fa0d99b2adc3e3c1acf5aae5a3939
Jindrich Mynarz <mynarzjindrich@gmail.com>
2017-04-13 15:55:25 +0200

    Test if data is returning

--- src/matchmaker_sparql/evaluation/fold/teardown.clj
+++ src/matchmaker_sparql/evaluation/fold/teardown.clj
@@ -5,6 +5,13 @@
             [stencil.core :as stencil]
             [taoensso.timbre :as timbre]))
 
+(defn- evaluation-data-empty?
+  "Test if the `evaluation-graph` is empty."
+  [evaluation-graph]
+  (let [query (stencil/render-file "templates/evaluation/teardown/evaluation_data_empty"
+                                   {:evaluation-graph evaluation-graph})]
+    (assert (not (sparql/ask-query endpoint query)) "Evaluation data not empty!")))
+
 (defn return-matches
   "Return matches used in evaluation to the source `graph`."
   [{:keys [evaluation-graph graph]}]
@@ -12,4 +19,5 @@
   (let [operation (stencil/render-file "templates/evaluation/teardown/return_matches"
                                        {:evaluation-graph evaluation-graph
                                         :graph graph})]
-    (sparql/update-operation endpoint operation)))
+    (sparql/update-operation endpoint operation)
+    (evaluation-data-empty? evaluation-graph)))
--- src/matchmaker_sparql/evaluation/teardown.clj
+++ src/matchmaker_sparql/evaluation/teardown.clj
@@ -15,6 +15,15 @@
     (timbre/info "Returning reduced data...")
     (sparql/update-operation endpoint update-operation)))
 
+(defn- withheld-data-empty?
+  "Test if the `withheld-graph` is empty (after returning the withheld data)."
+  [withheld-graph]
+  (let [query (stencil/render-file "templates/evaluation/teardown/withheld_data_empty"
+                                   {:withheld-graph withheld-graph})]
+    (assert (not (sparql/ask-query endpoint query)) "Withheld data not empty!")))
+
 (defn teardown-evaluation
   [{:keys [data-reduced? withheld-graph]}]
-  (when data-reduced? (return-withheld-data withheld-graph)))
+  (when data-reduced?
+    (return-withheld-data withheld-graph)
+    (withheld-data-empty? withheld-graph)))
